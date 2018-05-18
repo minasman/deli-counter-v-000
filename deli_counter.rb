9cf436a553a3a9ca82921526db28964a431c81e1
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 00:50:32 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ def now_serving(line)
   if line.length == 0 
     puts "There is nobody waiting to be served!"
   else
-    serving = line.pop
+    serving = line.unshift
     puts "Currently serving #{serving}."
     return line
   end
