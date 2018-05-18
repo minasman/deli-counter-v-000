e4b4331d632f2fbf0824ab341e736b4c96164dcb
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 00:30:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,7 +4,8 @@ def line(guests)
   if guests.size == 0 
     puts "The line is currently empty."
   else 
-    
+    guests.each_with_index do |name, i|
+      
 end
 
 def take_a_number(line, name)
