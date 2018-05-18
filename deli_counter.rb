e704b24266ff2d67e5aaf1d5ec7c8333a39bf56c
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 00:29:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,15 @@
 # Write your code here.
 katz_deli = []
 def line(guests) 
-  
+  if guests.size == 0 
+    puts "The line is currently empty."
+  else 
+    
 end
 
 def take_a_number(line, name)
   line.push(name)
-  puts "Welcome#{name} "
+  puts "Welcome, #{name}. You are number #{line.size} in line."
 end
 
 def now_serving(line)
