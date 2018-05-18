6458a3d17fe80a373bff4b3f585fede7b1ee036e
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-18 00:41:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,7 +1,7 @@
 # Write your code here.
 katz_deli = []
 def line(guests) 
-  if guests.size == 0 
+  if guests.length == 0 
     puts "The line is currently empty."
   else 
     line = "The line is currently: "
